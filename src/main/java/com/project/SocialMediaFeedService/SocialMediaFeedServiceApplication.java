e4f82e4e0a474c07bad9d6fe98ffe0044323dc0a
Arjun-1111 <contactme.xyz1@gmail.com>
2026-08-01 12:41:47 +0530

    Initial commit

---
+++ src/main/java/com/project/SocialMediaFeedService/SocialMediaFeedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.SocialMediaFeedService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaFeedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaFeedServiceApplication.class, args);
+	}
+
+}
