779465b4b4153bdd779d11833a5bfd91137f8dc6
Arjun-1111 <contactme.xyz1@gmail.com>
2026-08-03 21:03:14 +0530

    BaseEntity and userEntity Created and approved

--- src/main/java/com/project/SocialMediaFeedService/user/entity/User.java
+++ src/main/java/com/project/SocialMediaFeedService/user/entity/User.java
@@ -1,4 +1,38 @@
 package com.project.SocialMediaFeedService.user.entity;
 
-public class User {
+import com.project.SocialMediaFeedService.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    String username;
+
+    @Column(nullable = false,unique = true, length = 100)
+    String email;
+
+    @Column(nullable = false, length = 100)
+    String password;
+
+    @Column(length = 500)
+    String bio;
+
+    @Column(name= "avatar_url")
+    String avatarUrl;
+
+    @Column(name = "follower_count", nullable = false, columnDefinition = "bigint default 0")
+    Long followerCount = 0L;
+
+    @Column(name = "following_count", nullable = false, columnDefinition = "bigint default 0")
+    Long followingCount = 0L;
+
 }
